c1ba6bd2056715698fb82d2fcdfbe8db0fa6261d
boo-standing-up <70996914+boo-standing-up@users.noreply.github.com>
2021-03-20 08:21:06 +0900

    ボタン装着

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const App = () => {
   return (
-    <React.Fragment>
+    <>
       <h1>おはよう！</h1>
       <p>お元気ですか？</p>
-    </React.Fragment>
+      <button>ボタン</button>
+    </>
   );
 };
 
