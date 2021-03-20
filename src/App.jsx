edd1820dfc6ffe94d67be070ec398a368140a301
boo-standing-up <70996914+boo-standing-up@users.noreply.github.com>
2021-03-20 21:39:10 +0900

    イベント設定終了

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+  const styleContent1 = {
+    color: "green",
+    fontSize: "30px"
+  };
+
   return (
     <>
-      <h1>おはよう！</h1>
-      <p>お元気ですか？</p>
-      <button>ボタン</button>
+      <h1 style={{ color: "red" }}>おはよう！</h1>
+      <p style={styleContent1}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
